aadf64081d60545984f62abd095462e7eb9a1244
Andrew Meshkov <meshkov89@gmail.com>
2024-08-29 11:31:01 +0300

    feat:  сщьздуфе ефыл

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.min + this.max) / 2)
     }
 
     lower() {
-
+        if (this.min < this.max) {
+            this.max = Math.round((this.min + this.max) / 2)
+        }
     }
 
     greater() {
-
+        if (this.min < this.max) {
+            this.min = Math.round((this.min + this.max) / 2)
+        }
     }
 }
 
